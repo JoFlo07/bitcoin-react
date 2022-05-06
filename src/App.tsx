44027a1883379a67e3525624637559f3839fb97f
fjost <jost@mobile-software.ag>
2022-05-06 17:57:19 +0200

    style: app wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { BTCConverterPage } from "./pages/btc-converter/BTCConverterPage";
 import { BTCDetailsPage } from "./pages/btc-details/BTCDetailsPage";
 import { BTCStatisticsPage } from "./pages/btc-statistics/BTCStatisticsPage";
 import { DashboardPage } from "./pages/dashboard/DashboardPage";
+import { SideMenu } from "./shared/components/side-menu/side-menu";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<DashboardPage />}></Route>
-      <Route path="/statistics" element={<BTCStatisticsPage />}></Route>
-      <Route path="/converter" element={<BTCConverterPage />}></Route>
-      <Route path="/details" element={<BTCDetailsPage />}></Route>
-    </Routes>
+    <div className="app-wrapper">
+      <nav className="side-content">
+        <SideMenu />
+      </nav>
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<DashboardPage />}></Route>
+          <Route path="/statistics" element={<BTCStatisticsPage />}></Route>
+          <Route path="/converter" element={<BTCConverterPage />}></Route>
+          <Route path="/details" element={<BTCDetailsPage />}></Route>
+        </Routes>
+      </div>
+    </div>
   );
 }
 
