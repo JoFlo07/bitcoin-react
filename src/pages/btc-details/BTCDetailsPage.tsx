337f76b0d2223de45a42d3f25ef0a0857ea04cfe
fjost <jost@mobile-software.ag>
2022-05-07 08:27:16 +0200

    feat: load data

--- src/pages/btc-details/BTCDetailsPage.tsx
+++ src/pages/btc-details/BTCDetailsPage.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import { HeaderBar } from "../../shared/components/header-bar/header-bar";
+import { BTCStatistic } from "../../shared/models/interfaces";
+import { getBTCStats } from "../../shared/services/api-service";
 
 export const BTCDetailsPage = () => {
   const pageTitle = "Details";
+  const [btcStats, setBtcStats] = useState<BTCStatistic>();
+
+  useEffect(() => {
+    const fetchBtcStats = async () => {
+      const stats = await getBTCStats();
+      if (stats) {
+        console.log(stats);
+        setBtcStats(stats);
+      }
+    };
+
+    fetchBtcStats();
+  }, []);
+  
   return <HeaderBar title={pageTitle} />;
 };
--- src/shared/services/api-service.ts
+++ src/shared/services/api-service.ts
@@ -1,6 +1,8 @@
-import { ExChangeRate } from "../models/interfaces";
+import { BTCStatistic, ExChangeRate } from "../models/interfaces";
 // EXCHANGE RATE API
 const exchangeRateApi = "https://blockchain.info/ticker";
+// BTC STATS API
+const btcStatsApi = "https://api.blockchain.info/stats";
 
 export const getExchangeRates = async () => {
   try {
@@ -11,3 +13,13 @@ export const getExchangeRates = async () => {
     console.error("Error loading exchange rate data", error);
   }
 };
+
+export const getBTCStats = async () => {
+  try {
+    const response = await fetch(btcStatsApi);
+    const btcStatsData = (await response.json()) as BTCStatistic;
+    return btcStatsData;
+  } catch (error) {
+    console.error("Error loading btc stats", error);
+  }
+}
