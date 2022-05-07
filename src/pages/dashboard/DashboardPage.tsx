e468f4c8454cc3bae6307f6de73ee96b55d060ea
fjost <jost@mobile-software.ag>
2022-05-07 07:45:26 +0200

    feat: add grid

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,3 +1,4 @@
+import { Container, Grid } from "@mui/material";
 import { useEffect, useState } from "react";
 import { HeaderBar } from "../../shared/components/header-bar/header-bar";
 import { ExChangeRate } from "../../shared/models/interfaces";
@@ -24,21 +25,27 @@ export const DashboardPage = () => {
     return keys.map((key) => {
       const buyValue = exchangeRatesInfo[key].buy.toString();
       const sellValue = exchangeRatesInfo[key].buy.toString();
-      const currencyName =  exchangeRatesInfo[key].symbol;
+      const currencyName = exchangeRatesInfo[key].symbol;
       return (
-        <BTCExchangeRateCard
-          key={key}
-          buyValue={buyValue}
-          sellValue={sellValue}
-          currencyName={currencyName}
-        />
+        <Grid item xs={3}>
+          <BTCExchangeRateCard
+            key={key}
+            buyValue={buyValue}
+            sellValue={sellValue}
+            currencyName={currencyName}
+          />
+        </Grid>
       );
     });
   };
   return (
     <>
       <HeaderBar title={pageTitle} />
-      {renderExchangeRateCards(exChangeRates)}
+      <Container sx={{ overflow: "auto", height: '100%', paddingY: 5, }}>
+        <Grid container spacing={2} sx={{ marginBottom: 20 }}>
+          {renderExchangeRateCards(exChangeRates)}
+        </Grid>
+      </Container>
     </>
   );
 };
