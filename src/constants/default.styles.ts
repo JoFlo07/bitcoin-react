d086e9681b1a747dc539b6e41fed3b3d5cd4d975
fjost <jost@mobile-software.ag>
2022-05-07 15:08:30 +0200

    feat: fix mobile view

--- src/constants/default.styles.ts
+++ src/constants/default.styles.ts
@@ -25,3 +25,34 @@ export const paperItem: SxProps<Theme> = {
   alignItems: "flex-start",
   padding: 2,
 };
+
+export const converterGrid: SxProps<Theme> = {
+  marginY: 10,
+  marginX: 2,
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center",
+  paddingY: 5,
+  paddingX: 1,
+  border: "1px solid black",
+  borderRadius: 5,
+};
+export const converterGridMobile: SxProps<Theme> = {
+  ...converterGrid,
+  flexDirection: "column",
+};
+
+export const convertedBTCField: SxProps<Theme> = {
+  border: "1px solid #000000DE",
+  width: 200,
+  height: 50,
+  borderRadius: 4,
+};
+
+export const convertedBTCText: SxProps<Theme> = {
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  paddingRight: 2,
+  height: "100%",
+};
--- src/pages/btc-converter/BTCConverterPage.tsx
+++ src/pages/btc-converter/BTCConverterPage.tsx
@@ -1,24 +1,31 @@
 import {
-  Box,
-  Container,
+  Box, Grid,
   MenuItem,
   OutlinedInput,
   Select,
   SelectChangeEvent,
-  Typography,
+  Typography
 } from "@mui/material";
 import { useState } from "react";
 import NumberFormat from "react-number-format";
+import {
+  convertedBTCField,
+  convertedBTCText,
+  converterGrid,
+  converterGridMobile
+} from "../../constants/default.styles";
 import { HeaderBar } from "../../shared/components/header-bar/header-bar";
+import useIsMobile from "../../shared/hooks/isMobile";
 import { convertCurrencyToBTC } from "../../shared/services/api-service";
 
 export const BTCConverterPage = () => {
-  const pageTitle = "Converter";
+  const pageTitle = "BTC Converter";
   const [displayAmount, setDisplayAmount] = useState<string>();
-  const [amount, setAmount] = useState<string>('');
+  const [amount, setAmount] = useState<string>("");
   const [selectedCurrency, setSelectedCurrency] = useState("EUR");
   const [convertedBTC, setConvertedBTC] = useState("");
   const currencyOptions = ["EUR", "USD", "AUD", "NZD", "GBP"];
+  const isMobile = useIsMobile();
 
   const handleSelect = async (event: SelectChangeEvent, amount: string) => {
     setSelectedCurrency(event.target.value);
@@ -27,78 +34,71 @@ export const BTCConverterPage = () => {
 
   const calculateBTCValue = async (currency: string, value: string) => {
     const valueAsNumber = Number(value);
-    const btcAmount = await convertCurrencyToBTC(
-      currency,
-      valueAsNumber
-    );
+    const btcAmount = await convertCurrencyToBTC(currency, valueAsNumber);
     setConvertedBTC(String(btcAmount));
   };
 
   return (
     <>
       <HeaderBar title={pageTitle} />
-      <Container
+      <Grid
+        container
         sx={{
-          marginTop: 10,
           display: "flex",
           justifyContent: "space-around",
           alignItems: "center",
-          width: "60%",
-          padding: 5,
-          border: "1px solid black",
-          borderRadius: 5,
         }}
       >
-        <Box>
-          <NumberFormat
-            allowNegative={false}
-            allowLeadingZeros={false}
-            customInput={OutlinedInput}
-            isNumericString={true}
-            thousandSeparator={true}
-            value={displayAmount}
-            decimalScale={2}
-            onValueChange={async (values) => {
-              const { formattedValue, value } = values;
-              setDisplayAmount(formattedValue);
-              setAmount(value);
-              await calculateBTCValue(selectedCurrency, value);
-            }}
-          />
-          <Select
-            value={selectedCurrency}
-            onChange={(e) => handleSelect(e, amount)}
-            sx={{ minWidth: 100 }}
-          >
-            {currencyOptions.map((opt) => (
-              <MenuItem key={opt} value={opt}>
-                {opt}
-              </MenuItem>
-            ))}
-          </Select>
-        </Box>
-        <Typography>=</Typography>
-        <Box
-          sx={{
-            border: "1px solid #000000DE",
-            width: 200,
-            height: 50,
-            borderRadius: 4,
-          }}
+        <Grid
+          item
+          lg={8}
+          md={10}
+          sm={12}
+          xs={12}
+          sx={isMobile ? converterGridMobile : converterGrid}
         >
-          <Typography
+          <Box
             sx={{
               display: "flex",
-              justifyContent: "flex-end",
-              alignItems: "center",
-              paddingRight: 2,
-              height: "100%",
             }}
           >
-            {convertedBTC} BTC
-          </Typography>
-        </Box>
-      </Container>
+            <NumberFormat
+              allowNegative={false}
+              allowLeadingZeros={false}
+              customInput={OutlinedInput}
+              isNumericString={true}
+              thousandSeparator={true}
+              value={displayAmount}
+              decimalScale={2}
+              onValueChange={async (values) => {
+                const { formattedValue, value } = values;
+                setDisplayAmount(formattedValue);
+                setAmount(value);
+                if (value) {
+                  await calculateBTCValue(selectedCurrency, value);
+                } else {
+                  setConvertedBTC("");
+                }
+              }}
+            />
+            <Select
+              value={selectedCurrency}
+              onChange={(e) => handleSelect(e, amount)}
+              sx={{ minWidth: 100 }}
+            >
+              {currencyOptions.map((opt) => (
+                <MenuItem key={opt} value={opt}>
+                  {opt}
+                </MenuItem>
+              ))}
+            </Select>
+          </Box>
+          <Typography>=</Typography>
+          <Box sx={convertedBTCField}>
+            <Typography sx={convertedBTCText}>{convertedBTC} BTC</Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
