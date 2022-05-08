9fa0c41470b2323d106f32be7391d1955cdcdb0a
fjost <jost@mobile-software.ag>
2022-05-08 07:25:40 +0200

    feat: load chart data

--- src/pages/btc-statistics/BTCStatisticsPage.tsx
+++ src/pages/btc-statistics/BTCStatisticsPage.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import { HeaderBar } from "../../shared/components/header-bar/header-bar";
+import { getBTCMarketPriceinUSD } from "../../shared/services/api-service";
 
 export const BTCStatisticsPage = () => {
   const pageTitle = "Statistics";
+  useEffect(() => {
+    const getChartData = async () => {
+      try {
+        const chartData = await getBTCMarketPriceinUSD("all");
+        console.log(chartData);
+      } catch (error) {
+        console.error("Error loading chart data", error);
+      }
+    };
+    getChartData();
+  }, []);
+  
   return <HeaderBar title={pageTitle} />;
 };
